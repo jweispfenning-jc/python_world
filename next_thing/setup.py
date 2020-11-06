f49d19482b29bda56b0b3ff1a11715303160027e
John Weispfenning <john.weispfenning@jumpcloud.com>
2020-11-06 14:40:48 -0700

    Changes

---
+++ next_thing/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name="python_world",
+    use_scm_version={
+        "root": "..",
+        "local_scheme": "node-and-timestamp",
+        "relative_to": __file__,
+    },
+    description="Python custom thingy",
+    author="John Weispfenning",
+    install_requires=[],
+    author_email="yourmom@blackhole.com",
+    packages=find_packages(),
+)
---
+++ python_world/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name="next_thing",
+    use_scm_version={
+        "root": "..",
+        "local_scheme": "node-and-timestamp",
+        "relative_to": __file__,
+    },
+    description="Old news",
+    author="John Weispfenning",
+    install_requires=[],
+    author_email="yourmom@blackhole.com",
+    packages=find_packages(),
+)
